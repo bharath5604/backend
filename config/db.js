b23c6f07fbc3e1510ccb01418117c7f8ca71e1a0
bharath5604 <bhaveshperumalla@gmail.com>
2025-12-31 22:19:04 +0530

    changes

--- config/db.js
+++ config/db.js
@@ -2,10 +2,23 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    const uri = process.env.MONGO_URI;
+    if (!uri) {
+      throw new Error('MONGO_URI is not set in environment variables');
+    }
+
+    // Optional: log which DB/cluster you are connecting to (without credentials)
+    try {
+      const safeUri = uri.replace(/:\/\/([^:]+):([^@]+)@/, '://<user>:<password>@');
+      console.log('Connecting to MongoDB at', safeUri);
+    } catch (_) {
+      console.log('Connecting to MongoDB...');
+    }
+
+    await mongoose.connect(uri);
     console.log('MongoDB connected');
   } catch (err) {
-    console.error(err.message);
+    console.error('MongoDB connection error:', err.message);
     process.exit(1);
   }
 };
